20e5eefa89c843a01e8ce92f6a9fd1cb55734491
vivek-netizen <83751874+vivek-netizen@users.noreply.github.com>
2023-01-14 20:47:28 +0530

    Add file via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+{
+   var buttonInhtml=this.innerHTML;
+   makesound(buttonInhtml);
+}
+);
+//  var audio=new Audio("sounds/tom-1.mp3");
+//     audio.play();
+}
+
+
+document.addEventListener("keydown",function(event){
+   makesound(event.key);  
+})
+
+function makesound(keycheck){
+   switch(keycheck){
+      case 'W':
+      case 'w':var tom1=new Audio("sounds/tom-1.mp3");
+               tom1.play();
+      break;  
+      case 'A':
+      case "a":var tom2=new Audio("sounds/tom-2.mp3");
+               tom2.play();
+               break;
+      case "S":        
+      case "s":var tom3=new Audio("sounds/tom-3.mp3");
+               tom3.play();
+               break;
+      case "D":
+      case "d":var tom4=new Audio("sounds/tom-4.mp3");
+               tom4.play();
+               break;
+      case "J":
+      case "j":var snare=new Audio("sounds/snare.mp3");
+               snare.play();
+               break;
+      case "K":         
+      case "k":var crash=new Audio("sounds/crash.mp3");
+               crash.play();
+               break;
+      case "L":         
+      case "l":var kickbass=new Audio("sounds/kick-bass.mp3");
+               kickbass.play();
+               break;
+      default:console.log("Wrong input!!!!!");
+}
+}
+
