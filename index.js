581df8460a2eee8d2bfbd90d3ca871706f55ad13
vivek <83751874+vbvivek28@users.noreply.github.com>
2023-02-02 18:10:32 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
-document.querySelectorAll(".drum")[i].addEventListener("touchstart",function ()
+document.querySelectorAll(".drum")[i].addEventListener("click",function ()
 {
    var buttonInhtml=this.innerHTML;
    makesound(buttonInhtml);
